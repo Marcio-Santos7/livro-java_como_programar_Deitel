7c80647bc99d0eb8297dad87f9e1203e0cafb91d
Marcio Leandro <marcio449@hotmail.com>
2019-06-25 22:32:00 -0300

    Criação da classe BasePlusCommissionEmployee

--- src/cap9/BasePlusCommissionEmployee.java
+++ src/cap9/BasePlusCommissionEmployee.java
@@ -29,4 +29,78 @@ public class BasePlusCommissionEmployee {
         setCommissionRate( rate ); // valida e armazena a taxa de comissão
         setBaseSalary( salary ); // valida e armazena o salário-base
     } // fim do construtor BasePlusCommisionEmployee de seis argumentos
-}
+    
+    // configura o nome
+    public void setFirstName( String first ){
+        firstName = first;
+    } // fim do método setFirstName
+    
+    // retorna o nome
+    public String getFirstName(){
+        return firstName;
+    } // fim do método getFirstName
+    
+    // configura o sobrenome
+    public void setLastName( String last ){
+        lastName = last;
+    } // fim do método setLastName
+    
+    // retorna o sobrenome
+    public String getLastName(){
+        return lastName;
+    } // fim do método getLastName
+    
+    // configura o SSN (que corresponde ao nosso CPF)
+    public void setSocialSecurityNumber( String ssn ){
+        socialSecurityNumber = ssn; // deve validar
+    } // fim do método setSocialSecurityNumber
+    
+    // retorna o SSN
+    public String getSocialSecurityNumber(){
+        return socialSecurityNumber;
+    } // fim do método getSocialSecurityNumber
+    
+    // configura a quantidadade de vendas brutas
+    public void setGrossSales( double sales ){
+        grossSales = ( sales < 0.0 ) ? 0.0 : sales;
+    } // fim do método setGrossSales
+    
+    // retorna a quantidade de vendas brutas
+    public double getGrossSales(){
+        return grossSales;
+    } // fim do método getGrossSales
+    
+    // configura a taxa de comissão 
+    public void setCommissionRate( double rate ){
+        commissionRate = ( rate > 0.0 && rate <1.0 ) ? rate: 0.0;
+    } // fim do método setCommissionRate
+    
+    // retorna a taxa de comissão
+    public double getCommisionRate(){
+        return commissionRate;
+    } // fim do método getCommission
+    
+    // configura o salário-base
+    public void setBaseSalary( double salary ){
+        baseSalary = ( salary < 0.0 ) ? 0.0 : salary;
+    } // fim do método setBaseSalary
+    
+    // retorna o salário-base
+    public double getBaseSalary(){
+        return baseSalary;
+    } // fim do método getBaseSalary
+    
+    // calcula os lucros
+    public double earnings(){
+        return baseSalary + ( commissionRate * grossSales );
+    } // fim do método earnings
+    
+    // retorna a representação de String de BasePlusCommissionEmployee
+    public String toString(){
+        return String.format( "%s: %s %s\n%s: %s\n%s: %.2f\n%s: %.2f\n%s: %.2f",
+                "base-salaried commission employee", firstName, lastName, 
+                "social security number", socialSecurityNumber,
+                "gross sales", grossSales, "commission rate", commissionRate,
+                "base salary", baseSalary );
+    } // fim do método toString    
+    } // fim da classe BasePlusCommissionEmployee
